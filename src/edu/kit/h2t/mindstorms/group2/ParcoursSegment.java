8a184eeca274d40756781c5b9db4b0cf2dbce65d
Leonardo Vasi <leonardo@vasi.de>
2021-11-15 13:08:23 +0100

    Rewrote Line Follow. Added Regulated Follower to Line Follower.

--- src/edu/kit/h2t/mindstorms/group2/ParcoursSegment.java
+++ src/edu/kit/h2t/mindstorms/group2/ParcoursSegment.java
@@ -1,6 +1,9 @@
 package edu.kit.h2t.mindstorms.group2;
 
 import lejos.utility.Delay;
+import lejos.ev3.tools.LCDDisplay;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.BaseRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
@@ -10,6 +13,9 @@ import lejos.robotics.RegulatedMotor;
 
 public enum ParcoursSegment {
 	LINEFOLLOW("Follow line") {
+		private final double p = 1000;
+		private final double offset = 0.3;
+		
 		private Port colorPort;
 		private EV3ColorSensor color;
 		private RegulatedMotor sensorMover;
@@ -19,63 +25,146 @@ public enum ParcoursSegment {
 			color = new EV3ColorSensor(colorPort);
 			sensorMover = new EV3MediumRegulatedMotor(MotorPort.D);
 			redMode = color.getRedMode();
-			ParcoursMain.leftMotor.setSpeed(250);
-			ParcoursMain.rightMotor.setSpeed(250);
+			ParcoursMain.leftMotor.setSpeed(360);
+			ParcoursMain.rightMotor.setSpeed(360);
 			sensorMover.setSpeed(3600);
 		}
 		public void doStep() {
 			//Drive on line
 			float res[] = new float[1];
-			ParcoursMain.leftMotor.synchronizeWith(new RegulatedMotor[]{ParcoursMain.rightMotor});
-			do {
-				redMode.fetchSample(res, 0);
+//			ParcoursMain.leftMotor.synchronizeWith(new RegulatedMotor[]{ParcoursMain.rightMotor});
+//			ParcoursMain.rightMotor.synchronizeWith(new RegulatedMotor[]{ParcoursMain.leftMotor});
+			
+			while (getRedValue() > 0.2) {
+				int y = (int) ((getRedValue() - offset) * p);
+				LCD.drawInt(y, 4, 6);
+				
+				ParcoursMain.leftMotor.setSpeed(180 + y);
+				ParcoursMain.rightMotor.setSpeed(180 - y);
 				ParcoursMain.leftMotor.forward();
 				ParcoursMain.rightMotor.forward();
-			} while (res[0] > 0.4);
-			ParcoursMain.leftMotor.stop();
+				
+				LCD.clear(6);
+				LCD.drawString("DO  " + Float.toString(res[0]).substring(0, 3), 4, 6);
+			}
+			
+			ParcoursMain.leftMotor.stop(true);
 			ParcoursMain.rightMotor.stop();
+			
 			
 			//Lost line, let's find it
 			float[] leftSample = new float[1];
 			float[] rightSample = new float[1];
 			
+			//Rotate to left
 			sensorMover.rotateTo(-60, false);
 			redMode.fetchSample(leftSample, 0);
 			
+			//Rotate to right
 			sensorMover.rotateTo(60, false);
 			redMode.fetchSample(rightSample, 0);
 			
+			//Compare Values of left and right
 			float diff = leftSample[0] - rightSample[0];
 			
 			sensorMover.rotateTo(0, false);
 			
+			LCD.clear(6);
+			LCD.drawString("LOST " + Float.toString(diff).substring(0, 3), 4, 6);
+			
+//			ParcoursMain.leftMotor.startSynchronization();
+			float eps = 0.15f;
+			
 			//line is left
-			if (diff < -0.1) {
-				ParcoursMain.leftMotor.stop();
+			if (diff < -eps) {
+				LCD.clear(6);
+				LCD.drawString("Links:  " + Float.toString(diff).substring(0, 3), 4, 6);
+				ParcoursMain.leftMotor.stop(true);
 				ParcoursMain.rightMotor.stop();
-				sensorMover.rotateTo(60, false);
+				sensorMover.rotateTo(0, false);
+				
 				do {
 					redMode.fetchSample(res, 0);
+				
+//					ParcoursMain.leftMotor.startSynchronization();
 					ParcoursMain.rightMotor.rotate(100, false);
+//					ParcoursMain.leftMotor.endSynchronization();
+				
 				} while (res[0] < 0.2);
+				
 				//ParcoursMain.leftMotor.rotate(-120, false);
-				sensorMover.rotateTo(0, false);
+//				sensorMover.rotateTo(0, false);
 			}
 			
 			//line is right
-			else if (diff > 0.1) {
-				ParcoursMain.leftMotor.stop();
+			else if (diff > eps) {
+				LCD.clear(6);
+				LCD.drawString("Rechts:  " + Float.toString(diff).substring(0, 3), 4, 6);
+				ParcoursMain.leftMotor.stop(true);
 				ParcoursMain.rightMotor.stop();
-				sensorMover.rotateTo(-60, false);
+				sensorMover.rotateTo(0, false);
+				
+
 				do {
 					redMode.fetchSample(res, 0);
+					
+//					ParcoursMain.leftMotor.startSynchronization();
 					ParcoursMain.leftMotor.rotate(30, true);
 					ParcoursMain.rightMotor.rotate(-100, false);
+//					ParcoursMain.leftMotor.endSynchronization();
+					
 				} while (res[0] < 0.2);
-				sensorMover.rotateTo(0, false);
+//				sensorMover.rotateTo(0, false);
 			}
+			
+			else {
+				LCD.clear(6);
+				LCD.drawString("Else:  " + Float.toString(diff).substring(0, 3), 4, 6);
+				ParcoursMain.leftMotor.stop(true);
+				ParcoursMain.rightMotor.stop();
+				
+				ParcoursMain.leftMotor.rotate(180, true);
+				ParcoursMain.rightMotor.rotate(180);
+				
+			}
+
+//			ParcoursMain.leftMotor.endSynchronization();
 			//ParcoursMain.moveTo(null);
 		}
+		public void syncForward() {
+			ParcoursMain.leftMotor.resetTachoCount();
+			ParcoursMain.rightMotor.resetTachoCount();
+			
+			ParcoursMain.leftMotor.forward();
+			ParcoursMain.rightMotor.forward();
+			
+		}
+		
+		public void syncStop() {
+			ParcoursMain.rightMotor.stop(true);
+			ParcoursMain.leftMotor.stop();
+			
+			int rightTacho =  ParcoursMain.rightMotor.getTachoCount();
+			int leftTacho =  ParcoursMain.leftMotor.getTachoCount();
+			
+			int TachoDiff = rightTacho - leftTacho;
+			
+			
+			if (TachoDiff > 0) {
+				ParcoursMain.leftMotor.rotate(TachoDiff);
+			} else {
+				ParcoursMain.rightMotor.rotate(-TachoDiff);
+			}
+		}
+		
+		public float getRedValue() {
+			float res[] = new float[1];
+			
+			redMode.fetchSample(res, 0);
+			
+			return res[0];
+		}	
+		
 	},
 	COUNT("Count") {
 		private int cnt;
@@ -99,13 +188,46 @@ public enum ParcoursSegment {
 			ParcoursMain.lcd.drawString(Integer.toString(cnt), 4, 3);
 		}
 	},
+	 FORWARD("Drive forward") {
+		private int cnt;
+		public void init() { cnt = 0; }
+		public void doStep() {
+
+			ParcoursMain.leftMotor.synchronizeWith(new RegulatedMotor[]{ParcoursMain.rightMotor});
+			
+			ParcoursMain.leftMotor.setSpeed(360);
+			ParcoursMain.rightMotor.setSpeed(360);
+			
+			ParcoursMain.leftMotor.startSynchronization();
+			ParcoursMain.rightMotor.forward();
+			ParcoursMain.leftMotor.forward();
+			ParcoursMain.leftMotor.endSynchronization();
+			
+			Delay.msDelay(2000);
+			
+//			ParcoursMain.leftMotor.setSpeed(0);
+//			ParcoursMain.rightMotor.setSpeed(0);
+			
+//			ParcoursMain.leftMotor.startSynchronization();
+			ParcoursMain.rightMotor.stop(true);
+			ParcoursMain.leftMotor.stop();
+//			ParcoursMain.leftMotor.endSynchronization();			
+			
+			LCD.drawString("Rechts: " + ParcoursMain.leftMotor.getTachoCount(), 6, 4);
+			LCD.drawString("Links: " + ParcoursMain.rightMotor.getTachoCount(), 6, 5);
+			
+			
+			Delay.msDelay(5000);
+			
+		}
+	},
 	LINEFOLLOW_REGULATED("Follow line using regulator"){
 		private Port colorPort;
 		private EV3ColorSensor color;
 		private RegulatedMotor sensorMover;
 		private SensorMode redMode;
-		private final double p = 1;
-		private final double offset = 0.5;
+		private final double p = 1000;
+		private final double offset = 0.3;
 		public void init() {
 			colorPort = ParcoursMain.brick.getPort("S1");
 			color = new EV3ColorSensor(colorPort);
@@ -119,8 +241,12 @@ public enum ParcoursSegment {
 			float[] sample = new float[1];
 			redMode.fetchSample(sample, 0);
 			int y = (int) ((sample[0] - offset) * p);
-			ParcoursMain.leftMotor.setSpeed(360 + y);
-			ParcoursMain.rightMotor.setSpeed(360 - y);
+			LCD.drawInt(y, 4, 6);
+			
+			ParcoursMain.leftMotor.setSpeed(180 + y);
+			ParcoursMain.rightMotor.setSpeed(180 - y);
+			ParcoursMain.leftMotor.forward();
+			ParcoursMain.rightMotor.forward();
 		}
 	};
 
