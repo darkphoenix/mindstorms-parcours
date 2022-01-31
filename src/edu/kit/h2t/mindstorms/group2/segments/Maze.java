88790215fc5b68ccf45078ea9f972b9bc69fd6f1
Leonardo Vasi <leonardo@vasi.de>
2022-01-31 11:23:38 +0100

    Reordered checks if both colors are found. Added abort.

--- src/edu/kit/h2t/mindstorms/group2/segments/Maze.java
+++ src/edu/kit/h2t/mindstorms/group2/segments/Maze.java
@@ -11,74 +11,76 @@ public class Maze implements ParcoursSegment {
 	private boolean foundWhite;
 	private boolean foundRed;
 	private boolean turnDirectionLeft;
-	
+
 	private boolean drivingForward = true;
-	
+
 	private int speedOffset = 25;
-	
+
 	private int leftTachoCache;
 	private int rightTachoCache;
-	
+
 	private float whiteEps = 0.2f;
 	private float redEps = 0.07f;
-	
+
 	public void init() {
 		foundRed = false;
 		foundWhite = false;
-		
+
 		turnDirectionLeft = true;
-		
+
 		drivingForward = true;
-		
+
 		RobotUtil.setMotorSpeed(RobotUtil.baseSpeed);
-		
-		while(RobotUtil.getDistance() > 0.15 && RobotUtil.chk()) {
+
+		while (RobotUtil.getDistance() > 0.15 && RobotUtil.chk()) {
 			RobotUtil.syncForward();
 		}
 //		RobotUtil.turn(1150, false);
 		RobotUtil.spin(600);
 	}
 
-	public void doStep() {		
+	public void doStep() {
 		LCD.drawString("White: " + foundWhite, 2, 2);
 		LCD.drawString("Red: " + foundRed, 2, 3);
-		
-		//driveRoutineTurns();
-		driveRoutineZigZag();
-		searchRoutine();
-		if(foundRed && foundWhite) {
+
+		if (foundRed && foundWhite) {
 			Sound.twoBeeps();
 //			RobotUtil.oneUpSound();
 			RobotUtil.syncStop();
+			RobotUtil.abort();
+		} else {
+			// driveRoutineTurns();
+			driveRoutineZigZag();
+			searchRoutine();
 		}
+
 	}
-	
 
 	private void searchRoutine() {
 		float[] color = RobotUtil.getRGB();
-		
+
 		float red = color[0];
 		float green = color[1];
 		float blue = color[2];
-		
+
 		RobotUtil.lcd.drawString("red: " + red, 2, 4);
 		RobotUtil.lcd.drawString("green: " + green, 2, 5);
 		RobotUtil.lcd.drawString("blue: " + blue, 2, 6);
-		
-		if(isRed(color) && !isWhite(color))  {
-			//Beep on first sound
-			if(!foundRed) {
+
+		if (isRed(color) && !isWhite(color)) {
+			// Beep on first sound
+			if (!foundRed) {
 				Sound.beep();
 			}
 			foundRed = true;
-		} 
-		
-		if(isWhite(color)) {
-			//Beep on first found
+		}
+
+		if (isWhite(color)) {
+			// Beep on first found
 			if (!foundWhite) {
 				Sound.beep();
 			}
-			
+
 			foundWhite = true;
 		}
 	}
@@ -87,22 +89,22 @@ public class Maze implements ParcoursSegment {
 		float red = rgb[0];
 		float green = rgb[1];
 		float blue = rgb[2];
-		
-		return blue > whiteEps && green > whiteEps;		
+
+		return blue > whiteEps && green > whiteEps;
 	}
-	
+
 	private boolean isRed(float[] rgb) {
 		float red = rgb[0];
 		float green = rgb[1];
 		float blue = rgb[2];
-		
+
 		return red > redEps && green < redEps;
 	}
-	
+
 	private void driveRoutineZigZag() {
-		
-		if (drivingForward){
-			if(RobotUtil.getDistance() < 0.15) {
+
+		if (drivingForward) {
+			if (RobotUtil.getDistance() < 0.15) {
 				drivingForward = false;
 				RobotUtil.syncBackward();
 				return;
@@ -111,9 +113,9 @@ public class Maze implements ParcoursSegment {
 				RobotUtil.syncForward();
 				return;
 			}
-			
+
 		} else if (!drivingForward) {
-			if(RobotUtil.getTouch()) {
+			if (RobotUtil.getTouch()) {
 				drivingForward = true;
 				Delay.msDelay(1000);
 				RobotUtil.syncForward();
@@ -124,35 +126,35 @@ public class Maze implements ParcoursSegment {
 				return;
 			}
 		}
-		
+
 	}
-	
+
 	private void driveRoutineTurns() {
-		if(RobotUtil.getDistance() < 0.15) {
-			//Backoff
+		if (RobotUtil.getDistance() < 0.15) {
+			// Backoff
 			RobotUtil.rightMotor.rotate(-100, true);
 			RobotUtil.leftMotor.rotate(-100, false);
-			
-			if(turnDirectionLeft) {
-				//Turn left
+
+			if (turnDirectionLeft) {
+				// Turn left
 //				RobotUtil.turn(2400, false);
 				RobotUtil.spin(1200);
 			} else {
-				//Turn right
+				// Turn right
 //				RobotUtil.turn(2400, true);
 				RobotUtil.spin(-1200);
 			}
-		
+
 			turnDirectionLeft = !turnDirectionLeft;
-			
-			while(!RobotUtil.getTouch() && RobotUtil.chk()) {
+
+			while (!RobotUtil.getTouch() && RobotUtil.chk()) {
 				searchRoutine();
 				RobotUtil.syncBackward();
 			}
-			
+
 		}
-		
+
 		RobotUtil.syncForward();
 	}
-	
+
 }
